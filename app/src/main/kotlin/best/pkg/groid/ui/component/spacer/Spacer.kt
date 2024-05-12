d8dd892ba2e04e68889e2f0d6fd8ec790b8f9c50
hiifong <i@hiif.ong>
2024-05-12 15:52:01 +0800

    Update

---
+++ app/src/main/kotlin/best/pkg/groid/ui/component/spacer/Spacer.kt
@@ -0,0 +1,18 @@
+package best.pkg.groid.ui.component.spacer
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+
+@Composable
+fun HeightSpacer(value: Dp) {
+    Spacer(modifier = Modifier.height(value))
+}
+
+@Composable
+fun WidthSpacer(value: Dp) {
+    Spacer(modifier = Modifier.width(value))
+}
--- app/src/main/kotlin/best/pkg/groid/ui/screen/LoginScreen.kt
+++ app/src/main/kotlin/best/pkg/groid/ui/screen/LoginScreen.kt
@@ -1,11 +1,8 @@
 package best.pkg.groid.ui.screen
 
-import android.util.Log
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,27 +11,39 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import best.pkg.groid.R
-import best.pkg.groid.TAG
+import best.pkg.groid.ui.component.spacer.HeightSpacer
+import best.pkg.groid.ui.component.spacer.WidthSpacer
+import best.pkg.groid.ui.theme.h1
+import best.pkg.groid.ui.theme.smileySansFamily
 import coil.compose.AsyncImage
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
     Column(
         modifier = modifier
-            .border(2.dp, Color.Cyan)
-            .padding(5.dp)
             .fillMaxHeight()
-            .height(200.dp),
+            .height(200.dp)
+            .paint(painterResource(id = R.drawable.background), contentScale = ContentScale.Crop),
         verticalArrangement = Arrangement.Center
     ) {
         AsyncImage(
@@ -47,38 +56,45 @@ fun LoginScreen(modifier: Modifier = Modifier) {
                 .clip(RoundedCornerShape(50.dp)),
         )
         Column(
-            modifier = modifier.fillMaxWidth(),
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(10.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
+            TextField(
+                value = username,
+                onValueChange = { username = it},
+                placeholder = {
+                    Text(
+                        text = "请输入用户名",
+                        style = h1,
+                        color = Color.Gray
+                    )
+                }
+            )
+            HeightSpacer(value = 10.dp)
+            TextField(
+                value = password,
+                onValueChange = { password = it},
+                placeholder = {
+                    Text(
+                        text = "请输入密码",
+                        style = h1,
+                        color = Color.Gray
+                    )
+                }
+            )
             Row(
-                modifier = modifier
-                    .fillMaxWidth()
-                    .height(50.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = stringResource(id = R.string.username) + ":")
-            }
-            Row(
-                modifier = modifier
-                    .fillMaxWidth()
-                    .height(50.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = stringResource(id = R.string.password) + ":")
-            }
-            Row(
-                modifier = modifier
-                    .height(50.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Button(onClick = {
-                    Log.d(TAG, "Login: haha")
                 }) {
-                    Text(text = "登录")
+                    Text(text = stringResource(id = R.string.login), fontFamily = smileySansFamily)
                 }
-                Spacer(modifier = Modifier.width(20.dp))
+                WidthSpacer(value = 20.dp)
                 Button(onClick = { /*TODO*/ }) {
-                    Text(text = "注册")
+                    Text(text = stringResource(id = R.string.register), fontFamily = smileySansFamily)
                 }
             }
         }
